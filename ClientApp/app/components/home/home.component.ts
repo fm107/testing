9967ade6c65b44702896ce0462c1240fde94cb7b
Alexander <fm107@ya.ru>
2017-04-24 00:47:51 +0300

    .

--- ClientApp/app/components/home/home.component.ts
+++ ClientApp/app/components/home/home.component.ts
@@ -10,24 +10,43 @@ import { IMessage } from "../data-service/message";
 
 declare var lity: any;
 
+
+import { TdDataTableService, TdDataTableSortingOrder, ITdDataTableSortChangeEvent, ITdDataTableColumn } from "@covalent/core";
+import { IPageChangeEvent } from "@covalent/core";
+
 @Component({
     selector: "home",
     templateUrl: "./home.component.html",
-    styleUrls: ["./home.component.css"]
+    styleUrls: ["./home.component.css"], providers: [TdDataTableService]
 })
 
 export class HomeComponent implements OnInit {
     fileSystemContent: IFileSystem[];
     parent: string;
     currentFolder: string;
+    filteredData: any[] = this.fileSystemContent;
+    searchTerm: string;
+    sortBy: string;
+    sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
 
     private messages: IMessage[] = new Array();
 
-    public messagesObs: Subject<IMessage>;
+    messagesObs: Subject<IMessage>;
 
     constructor(private http: Http,
         private data: DataService,
-        private wsService: WebSocketService) {
+        private wsService: WebSocketService,
+        private dataTableService: TdDataTableService) {
+    }
+
+    private name: ITdDataTableColumn = {
+        name: "name", label: "NAME #", tooltip: "Folder or file name", sortable: true
+    }
+    private size: ITdDataTableColumn = {
+        name: "size", label: "SIZE", tooltip: "Folder or file size", sortable: true, numeric: true, format: v => v.toFixed(2)
+    }
+    private changed: ITdDataTableColumn = {
+        name: "lastChanged", label: "LAST CHANGED", tooltip: "Folder or file last changed date", sortable: true, numeric: true
     }
 
     ngOnInit() {
@@ -40,7 +59,7 @@ export class HomeComponent implements OnInit {
                 const msg = JSON.parse(response.data);
                 return {
                     message: msg.message
-                }
+                };
             }) as Subject<IMessage>);
 
 
@@ -68,6 +87,49 @@ export class HomeComponent implements OnInit {
             this.fileSystemContent = res.contents;
             this.parent = res.parent;
             this.currentFolder = res.currentFolder;
+            this.filter(null);
         });
     }
+
+
+    sort(sortEvent: ITdDataTableSortChangeEvent): void {
+        this.sortBy = sortEvent.name;
+        this.sortOrder = sortEvent.order === TdDataTableSortingOrder.Ascending ?
+            TdDataTableSortingOrder.Descending : TdDataTableSortingOrder.Ascending;
+        console.log(sortEvent);
+        this.filter("sort");
+    }
+
+    search(searchTerm: string): void {
+        this.searchTerm = searchTerm;
+        this.filter("filter");
+    }
+
+
+    filter(action: string): void {
+        const newData: any[] = this.fileSystemContent;
+
+        switch (action) {
+            case "filter":
+                this.filteredData = this.dataTableService.filterData(newData, this.searchTerm, true);
+                break;
+            case "sort":
+                this.filteredData = this.dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+                break;
+            default:
+                this.filteredData = newData;
+                break;
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
