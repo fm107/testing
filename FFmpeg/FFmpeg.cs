c55f741bb711d9e3f00c97abb639d1a60338bb10
Alexander <fm107@ya.ru>
2017-06-27 23:19:47 +0300

    .

--- FFmpeg/FFmpeg.cs
+++ FFmpeg/FFmpeg.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
@@ -17,20 +16,29 @@ namespace WebTorrent
 
         public void CreatePlayList(string fileToConvert, string outputPath, string playList)
         {
-            Task.Factory.StartNew(() =>
-            {
-                var arguments = string.Format(@"-fflags +genpts -i ""{0}"" -codec copy -map 0 -f segment -segment_time 10 -segment_format mpegts -segment_list_flags live -segment_list ""{1}/{2}.m3u8"" -segment_list_type m3u8 ""{1}/{2}.%d.ts""",
-                    fileToConvert, outputPath, playList);
-
-                var processInfo = new ProcessStartInfo(_ffmpegSettings.FilePath)
+            Task.Factory.StartNew(() => CreatePlayListProcess(fileToConvert, outputPath, playList, true))
+                .ContinueWith(task =>
                 {
-                    Arguments = arguments
-                };
-
-                var process = Process.Start(processInfo);
-                process.WaitForExit();
-            });
+                    if (task.Result.ExitCode != 0)
+                        CreatePlayListProcess(fileToConvert, outputPath, playList, false);
+                });
         }
+
+        private Process CreatePlayListProcess(string fileToConvert, string outputPath, string playList, bool copyCodec)
+        {
+            var processInfo = new ProcessStartInfo(_ffmpegSettings.FilePath)
+            {
+                Arguments = string.Format(copyCodec
+                        ? @"-i ""{0}"" -codec copy -codec:a aac -map 0 -f segment -segment_time 10 -segment_format mpegts -segment_list_flags live -segment_list ""{1}/{2}.m3u8"" -segment_list_type m3u8 ""{1}/{2}.%d.ts"""
+                        : @"-i ""{0}"" -codec:v libx264 -codec:a aac -map 0 -f segment -segment_time 10 -segment_format mpegts -segment_list_flags live -segment_list ""{1}/{2}.m3u8"" -segment_list_type m3u8 ""{1}/{2}.%d.ts""",
+                    fileToConvert, outputPath, playList)
+            };
+
+            var process = Process.Start(processInfo);
+            process.WaitForExit();
+            return process;
+        }
+
 
         //Todo review
         private void ConvertVideo(object state)
