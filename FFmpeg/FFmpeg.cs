611b8d404a6b25a6ab2bcd4395c03fd2f92fce5f
Alexander <fm107@ya.ru>
2017-07-02 12:17:58 +0300

    Update FFmpeg.cs

--- FFmpeg/FFmpeg.cs
+++ FFmpeg/FFmpeg.cs
@@ -29,7 +29,7 @@ namespace WebTorrent
             var processInfo = new ProcessStartInfo(_ffmpegSettings.FilePath)
             {
                 Arguments = string.Format(copyCodec
-                        ? @"-i ""{0}"" -map 0 -c:a:0 aac -codec copy -f segment -segment_list_type m3u8 -segment_time 10 -segment_format mpegts -segment_list_flags +live -segment_list ""{1}/{2}.m3u8"" ""{1}/{2}.%d.ts"""
+                        ? @"-i ""{0}"" -map 0:0 -map 0:1 -codec copy -f segment -segment_list_type m3u8 -segment_time 10 -segment_format mpegts -segment_list_flags +live -segment_list ""{1}/{2}.m3u8"" ""{1}/{2}.%d.ts"""
                         : @"-i ""{0}"" -c:v:0 libx264 -c:a:0 aac -preset ultrafast -profile:v baseline -level 3.0 -f segment -segment_time 10 -segment_format mpegts -segment_list_flags +live -segment_list ""{1}/{2}.m3u8"" -segment_list_type m3u8 ""{1}/{2}.%d.ts""",
                     fileToConvert, outputPath, playList)
             };
