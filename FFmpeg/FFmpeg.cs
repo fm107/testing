062815dbb9ea8e23eb9e46850779a5097f72552c
Alexander <fm107@ya.ru>
2017-07-29 12:02:29 +0300

    .

--- FFmpeg/FFmpeg.cs
+++ FFmpeg/FFmpeg.cs
@@ -18,7 +18,9 @@ namespace WebTorrent
             _log = log;
             _ffmpegSettings = ffmpegOptions.Value;
 
-            _factory = new TaskFactory(new LimitedConcurrencyLevelTaskScheduler(3));
+            _factory = Environment.ProcessorCount < 4
+                ? new TaskFactory(new LimitedConcurrencyLevelTaskScheduler(4))
+                : new TaskFactory(new LimitedConcurrencyLevelTaskScheduler(Environment.ProcessorCount));
         }
 
         public void CreatePlayList(string fileToConvert, string outputPath, string playList)
